6dafa62cfb437f50ba4ec257f3281ec8a0c562c1
Jordan Harband <ljharb@gmail.com>
2025-02-28 09:53:50 -0800

    move inline GHA scripts to separate files
    also lint and typecheck them

---
+++ scripts/cache.mjs
@@ -0,0 +1,51 @@
+import { readFile, writeFile } from 'fs/promises';
+import path from 'path';
+
+import { reproduce } from 'reproduce';
+import { compare as semverCompare, Range } from 'semver';
+
+const { PACKAGE: pkg, VERSIONS } = process.env;
+
+const pkgDir = path.join(process.cwd(), 'results', /** @type {string} */ (pkg));
+
+/** @typedef {reproduce.Version} Version */
+/** @typedef {reproduce.ReproduceResult} ReproduceResult */
+
+const versions = /** @type {Version[]} */ (
+	new Range(/** @type {string} */ (VERSIONS)).set.flat(1).map((x) => x.value)
+);
+
+const [
+	results,
+	existingData,
+] = /** @type {[ReproduceResult[], { [k in Version]: ReproduceResult[] }]} */ (
+	await Promise.all(/** @type {const} */ ([
+		reproduce(`${pkg}@${versions}`),
+		Promise.all(versions.map(async (v) => /** @type {const} */ ([
+			v,
+			/** @type {ReproduceResult[]} */ (JSON.parse(await readFile(path.join(pkgDir, v), 'utf8').catch(() => '[]'))),
+		]))).then(Object.fromEntries),
+	]))
+);
+
+await Promise.all(results.map(async (result) => {
+	const dataPath = path.join(pkgDir, result.package.version);
+	const existing = existingData[result.package.version];
+
+	existing.push(result);
+	existing.sort((a, b) => {
+		const dA = Number(new Date(a.timestamp));
+		const dB = Number(new Date(b.timestamp));
+		if (dA !== dB) {
+			return dA - dB;
+		}
+		const vv = semverCompare(a.package.version, b.package.version);
+		if (vv !== 0) {
+			return vv;
+		}
+
+		return semverCompare(a.reproduceVersion, b.reproduceVersion);
+	});
+
+	await writeFile(dataPath, JSON.stringify(existing, null, '\t'));
+}));
